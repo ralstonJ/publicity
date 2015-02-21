7dc5abb1b5b1a56c6db8278ffc996b167a0ce79d
Eddie <Eddie@Eddies-MacBook-Pro.local>
2015-02-21 13:40:33 -0500

    added landing page buttons

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20150221175203) do
 
-  create_table "champ_co", force: true do |t|
+  create_table "champ_co", force: :cascade do |t|
     t.integer  "champ_co_id"
     t.string   "champ_co_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "fund_stat", force: true do |t|
+  create_table "fund_stat", force: :cascade do |t|
     t.integer  "fund_id"
     t.integer  "project_id"
     t.integer  "fund_date"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20150221175203) do
     t.datetime "updated_at"
   end
 
-  create_table "project_breakdown", force: true do |t|
+  create_table "project_breakdown", force: :cascade do |t|
     t.integer  "breakdown_id"
     t.string   "breakdown_name"
     t.string   "breakdown_value"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 20150221175203) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
+  create_table "projects", force: :cascade do |t|
     t.integer  "project_id"
     t.string   "project_name"
     t.string   "start_date"
